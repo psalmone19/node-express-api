43cbae0b8bd5aa18b627c262d180ab71c2aad2a7
Egan Bernardino <eegun19@yahoo.com>
2016-08-10 12:23:26 -0700

    added GET/:id route

--- README.md
+++ README.md
@@ -29,11 +29,26 @@ app.get('/', function(req, res){
 // The middleWear logger is local, because it's being used as a second parameter inside the get method.
 ```
 app.get('/about', middleWear.logger, function(req, res){
+    res.send('<h1>Express About Page</h1>')
+})
 ```
 
 ************************************************
 // Why do we move code and export it and require it?
 
-// Touched a new file called middlewear.js and moved the custom middlewear there
+// Touched a new file called middlewear.js and moved the custom middlewear there, which I then exported and required in the server.js.
 
-// Which I then exported and required in the server.js
+// npm installed body-parser `npm install body-parser --save` and required it in server.js, using it globally with `app.use(bodyParser`).
+
+// Created 1st GET /todos & tested it with POSTMAN by creating Collection->Environment->Route in POSTMAN.
+
+
+
+
+
+
+
+
+
+
+
--- server.js
+++ server.js
@@ -2,15 +2,50 @@ var express = require('express')
 
 var app = express()// creating an express app
 var path = require('path')
-var PORT = process.env.PORT || 3000
+var bodyParser = require('body-parser')
+var PORT = process.env.PORT || 3000;
 var middleWear = require('./middleWear')
+
+var todos = [
+  {
+    id: 1,
+    description: 'Teach REST API.',
+    completed: false
+  },
+  {
+    id: 2,
+    description: 'Go eat a healthy lunch.',
+    completed: true
+  }
+]
 
 
 // This middleWear is now for the whole app!
 app.use(middleWear.requireAuthentication)
+app.use(bodyParser())
 
 app.get('/', function(req, res){
   res.send('<h1>Express To-do API</h1>')
+})
+
+app.get('/todos', function(req, res){
+  res.json(todos)
+})
+
+app.get('/todos/:id', function(req, res){
+// creating a variable that will hold id from params object
+  var todoId = parseInt(req.params.id)
+  var matchedTodo;
+  todos.forEach(function(todo){
+    if(todoId === todo.id) {
+      matchedTodo = todo
+    }
+  })
+    if(matchedTodo) {
+      res.json(matchedTodo)
+    } else {
+      res.status(404).send()
+    }
 })
 
 app.get('/about', middleWear.logger, function(req, res){
